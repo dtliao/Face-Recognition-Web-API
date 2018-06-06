fe611bece1e7ce0a826c836f47adc4291982155f
Daniel Liao <39768672+dtliao@users.noreply.github.com>
2018-06-06 17:18:59 +0800

    edit backend server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image')
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
-    password : '820501Dl',
+    password : '',
     database : 'smartbrain'
   }
 });
